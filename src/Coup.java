ab4aaba8c0649961536ea7eb4ef3e9dfbc409be9
derguinm <meder1906@gmail.com>
2021-05-21 11:59:18 +0200

    l'ajout de la fonction ajouter mouvement dans coup

--- src/Coup.java
+++ src/Coup.java
@@ -6,16 +6,16 @@ import java.awt.*;
 public class Coup {
     SequenceListe<Point> points;
     SequenceListe<Point> direction;
-    int type;//coup par approche ou par eloignement
+    SequenceListe<Integer> type;//coup par approche ou par eloignement
 
-    public Coup(SequenceListe<Point> point, SequenceListe<Point> direction, int type) {
+    public Coup(SequenceListe<Point> point, SequenceListe<Point> direction,SequenceListe<Point> type) {
         this.points = point;
         this.direction = direction;
         this.type = type;
 
     }
 
-    public jouer(Jeu j){
+    public void jouer(Jeu j){
         Iterateur<Point> itp=points.iterateur();
         Iterateur<Point> itd=direction.iterateur();
         Iterateur<Integer> itt=type.iterateur();
@@ -25,5 +25,22 @@ public class Coup {
             int typ=itt.prochain();
             j.deplacer(point,direc,typ);
         }
+    }
+    // ajoute le mouvement si le point n'est pas encore parcouru
+    public void ajouterMouv(Point p,Point direction,int type){
+        boolean exist=false;
+        Iterateur<Point> itp=points.iterateur();
+        while(!exist && itp.aProchain()){
+            Point point_courrant=itp.prochain();
+            if(point_courrant.x==p.x && point_courrant.y==p.y)
+                exist=true;
+        }
+        if(!exist)
+        {
+            points.insereQueue(p);
+            this.direction.insereQueue(direction);
+            this.type.insereQueue(type);
+        }
+
     }
 }
