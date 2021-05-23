d8e42d6580908ca3ce4c03bdd8ccf9977a7ca5d3
zak-el <62945041+zak-el@users.noreply.github.com>
2021-05-23 22:44:40 +0200

    Add files via upload

---
+++ testFanorona.java
@@ -0,0 +1,40 @@
+import java.util.Scanner ;
+import java.awt.*  ;
+
+public class testFanorona {
+	public static void main(String[] args) {
+		int x1, x2, y1, y2, d=0 ;
+		Jeu j = new Jeu() ; 
+		j.getPlateau().iniPlateau();
+		j.getPlateau().affiche();
+		
+		Scanner sc = new Scanner(System.in);
+		
+		
+		while (!j.FinJeu()) {
+			
+			while(d == 0){
+				System.out.println(" enter x y");
+				x1 = sc.nextInt();
+				y1 = sc.nextInt();
+				
+				System.out.println(" enter x2 y2");
+				x2 = sc.nextInt();
+				y2 = sc.nextInt();
+
+				j.jouer(new Point(x1,y1), new Point(x2,y2));
+				j.getPlateau().affiche();
+				d = sc.nextInt();
+				
+			}
+			d = 0 ;
+			j.changerJoueur();
+		}
+		
+		
+		
+		
+		
+	}
+
+}
